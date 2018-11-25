848aaeae3acd035947fb5467dc7af71611881488
world66 <world666>
2018-11-25 14:50:13 +0100

    add php support

---
+++ new/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="DE">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> <!--  Задается отображение согласно ширине устройсва, пользователь не может масштабировать, только скролл -->
+	<meta name="description" content="Ihr Kinderhaus in Bad Hersfeld. Hier finden Sie die wichtige Informationen über unsere Kindergarten."> <!--  Задается описание страницы сайта в поиске -->
+	<link rel="shortcut icon" href="ico/favicon.ico">
+	
+    <title>Kinderhaus in Bad Hersfeld</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+
+    <link href="css/main.css" rel="stylesheet"> 	<!-- ?????Нужно свой файл создавать или подключить bootstrap css?????? -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  
+	<body>
+		<?php
+			include('header.html');
+		?>
+		<div class="container">
+			<h2>Herzlich wilkommen!</h2>
+			<br>
+		</div>
+		<div class="container">
+			<div class = "row">	
+				<div class="col-md-8">
+					<p class="lead">Wir, „Das Kinderhaus“, sind ein kleiner, sympathischer Kindergarten mitten in Hersfeld, gegründet und getragen von einer Elterninitiative.</p>
+					<p class="lead"> Unsere 18 Kinder im Alter von 2-6 Jahren werden von drei engagierten Erzieherinnen betreut. Unser Kinderhaus ist ein Ort des Miteinanders, in dem die Kinder in warmer, familiärer Atmosphäre unbeschwerte Zeit erleben. Wir legen Wert auf individuelle, altersgerechte Förderung, auf eine Erziehung, die geprägt ist von Verständnis und Toleranz.</p>
+				</div>
+				<div class="col-md-4 ">
+					<img  src="img/main.png"  width="100%" class="img-fluid mx-auto" alt="Das Kinderhaus Bad Hersfeld" >
+				</div>
+			</div>
+		</div>
+	
+			<!-- FOOTER -->
+		<footer style="padding: 0" class="container">
+			<br>
+			<div class="container">
+				<p class="float-right"><a href="#">Back to top</a></p>
+				<p>&copy; 2017-2018 Das Kinderhaus, Hainstraße 10, 36251 Bad Hersfeld &middot; <a href="impressum.html">Impressum</a> &middot; <a href="haftungsausschluss.html">Haftungsausschluss</a></p>
+			</div>
+		</footer>
+	
+
+		
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
