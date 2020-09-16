7c25b97aa4f9f10b6fcb02527041ff3a703e7554
world66 <world666>
2020-09-16 20:45:05 +0200

    add meta description tags

--- layout.php
+++ layout.php
@@ -4,14 +4,16 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="keywords" content="Kindertagesstätte, Kitas, Kita, Kindergarten, Kinderkrippe, Bad Hersfeld">
 		<meta name="description" content="Ihr Kinderhaus in Bad Hersfeld. Hier finden Sie die wichtige Informationen über unsere Kindergarten.">
+		<meta name="author" content="Hann Stepnykh, hanna.stepnykh@gmail.com">
 		<link rel="shortcut icon" href="ico/favicon.ico">
 
 		<title>Kinderhaus in Bad Hersfeld</title>
 
-		<link href="css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="fontawesome/css/all.css">
-		<link href="css/main.css" rel="stylesheet">
+		<link rel="stylesheet"  href="css/main.css">
 		<script src="js/navigation.js"></script>
 	</head>
 	<body>
