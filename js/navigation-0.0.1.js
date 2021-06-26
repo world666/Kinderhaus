6561b483cd163e4ec8d1c5d2170ecba2c785fb1d
world66 <world666>
2021-06-26 08:22:21 +0200

    some jst tests functions

--- js/navigation-0.0.1.js
+++ js/navigation-0.0.1.js
@@ -27,6 +27,133 @@ async function linkHandler(href, activeId)
 	{
 		alert(error);
 	}
+	
+	//cookieExp();
+	//localStorageExp();
+	//sessionStorageExp();
+	//backgroundFetch();
+	//indexedDBExp();
+}
+
+function indexedDBExp()
+{
+	if (!window.indexedDB)
+	{
+		console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+		return;
+	}
+	
+	let openRequest = window.indexedDB.open("store", 1);
+	openRequest.onupgradeneeded = function()
+	{  	// no database, init database neccessary
+		// версия существующей базы данных меньше 2 (или база данных не существует)
+		let db = openRequest.result;
+		switch(db.version)
+		{   // database update needed
+			case 0: // database doesn't exists at all
+				break;
+			default: // client hase some old version
+				break;
+				
+	    }
+		if (!db.objectStoreNames.contains('books'))
+		{   // books doesn't exist
+			db.createObjectStore('books', {keyPath: 'id'}); // create books
+		}
+	};
+
+	openRequest.onerror = function()
+	{
+		console.error("Error", openRequest.error);
+	};
+
+	openRequest.onsuccess = function()
+	{
+		let db = openRequest.result;
+		db.onversionchange = function()
+		{   // some other tab wants to update database version, close current session to allow it 
+			db.close();
+			alert("Old databse version, please reload page")
+		};
+		
+		let transaction = db.transaction("books", "readwrite");
+		let books = transaction.objectStore("books"); // (2)
+		let book =
+		{
+		  id: 'js',
+		  price: 10,
+		  created: new Date()
+		};
+		
+		let request = books.put(book); // (3)
+		request.onsuccess = function()
+		{
+			console.log("Book was added to the indexedDB", request.result);
+			// get one book
+			//let readRequest = books.get('js')
+			
+			// get key
+			let readRequest = books.getKey('js')
+			
+			// get books with key 'css' < id < 'html'
+		    //let readRequest = books.getAll(IDBKeyRange.bound('css', 'html'))
+
+			// get books with key 'html' <= id
+			//let readRequest = books.getAll(IDBKeyRange.lowerBound('html', true))
+
+			// get all books
+			//let readRequest = books.getAll()
+
+			// get all keys: id >= 'js'
+			//let readRequest = books.getAllKeys(IDBKeyRange.lowerBound('js', true))
+			
+			readRequest.onsuccess = function()
+			{
+				if (readRequest.result !== undefined)
+				{
+					console.log("books:", readRequest.result);
+				}
+				else
+				{
+					console.log("no books");
+				}
+		};
+
+		};
+		request.onerror = function()
+		{
+			console.log("Error", request.error);
+		};
+	};
+	
+	openRequest.onblocked = function()
+	{
+		// We have other connection to the same database with old version
+		// and it wasn't clased in db.onversionchange
+	};
+}
+
+function localStorageExp()
+{
+	myStorage = window.localStorage;
+	myStorage.setItem('prop1', 1);
+}
+
+function sessionStorageExp()
+{
+	myStorage = window.sessionStorage;
+	myStorage.setItem('prop1', 1);
+	myStorage.setItem(12, 1);
+	
+	let user = { name: "John", age: 30 };
+	myStorage.setItem("obj", JSON.stringify(user));
+	
+	let obj = JSON.parse(myStorage.getItem("obj"));
+}
+
+function cookieExp()
+{
+	document.cookie = "user=John";
 }
 
 function notifyMe()
@@ -60,7 +187,7 @@ function notifyMe()
 		});
 	}
 	else if (Notification.permission !== 'denied' || Notification.permission === "default")
-	{ // Otherwise, we need to ask the user for permission
+	{   // Otherwise, we need to ask the user for permission
 		Notification.requestPermission(function (permission)
 		{
 			if (permission === "granted") // If the user accepts, let's create a notification
